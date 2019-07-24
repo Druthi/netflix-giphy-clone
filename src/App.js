481ed83f6714bfffade57070c3a32620a13e6340
Druthi <polisettydruthi.k@gmail.com>
2019-07-24 18:49:38 -0500

    React setup complete

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component} from "react";
+import css from './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='app'>
+        HELLO
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/src');
+var DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+};
